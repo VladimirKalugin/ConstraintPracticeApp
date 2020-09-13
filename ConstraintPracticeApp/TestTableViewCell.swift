6cb29b4082d71ece32f77a485002b0dd1356a6fc
Embler <vbembler@gmail.com>
2020-09-13 10:30:15 +0300

    test

---
+++ ConstraintPracticeApp/TestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.swift
+//  ConstraintPracticeApp
+//
+//  Created by Vladimir on 13/09/2020.
+//  Copyright © 2020 myself. All rights reserved.
+//
+
+import UIKit
+
+class TestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
